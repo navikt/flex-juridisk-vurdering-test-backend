0a1e8cb3ef40a4cb6ae44e1bed9982e3a23334ad
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-01-24 17:26:31 +0100

    Testdata reset

---
+++ src/main/kotlin/no/nav/helse/flex/juridiskvurdering/TestdataResetListener.kt
@@ -0,0 +1,26 @@
+package no.nav.helse.flex.juridiskvurdering
+
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+@Component
+class TestdataResetListener(val juridiskVurderingRepository: JuridiskVurderingRepository) {
+
+    val log = logger()
+
+    @KafkaListener(
+        topics = [TESTDATA_RESET_TOPIC],
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        properties = ["auto.offset.reset = latest"],
+    )
+    fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+        val fnr = cr.value()
+        val antall = juridiskVurderingRepository.deleteByFnr(fnr)
+        log.info("Slettet $antall soknader på fnr $fnr - Key ${cr.key()}")
+        acknowledgment.acknowledge()
+    }
+}
+
+const val TESTDATA_RESET_TOPIC = "flex.testdata-reset"
