3ded2c0fe898d4347a6bc6b11f5297d03872eb9d
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-01-24 16:57:45 +0100

    API

---
+++ src/main/kotlin/no/nav/helse/flex/juridiskvurdering/JuridiskVurderingApi.kt
@@ -0,0 +1,20 @@
+package no.nav.helse.flex.juridiskvurdering
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = ["/api"])
+class JuridiskVurderingApi(
+    private val juridiskVurderingRepository: JuridiskVurderingRepository,
+) {
+
+    @ResponseBody
+    @GetMapping(value = ["/vurderinger/{fnr}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun hentVurderinger(fnr: String): List<JuridiskVurderingDbRecord> {
+        return juridiskVurderingRepository.findByFnr(fnr)
+    }
+}
