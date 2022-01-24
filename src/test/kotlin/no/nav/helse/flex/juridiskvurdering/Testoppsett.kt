2683498a5ec6b43d5c54fb45034177386e8e0829
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-01-24 16:27:38 +0100

    testfix

--- src/test/kotlin/no/nav/helse/flex/juridiskvurdering/Testoppsett.kt
+++ src/test/kotlin/no/nav/helse/flex/juridiskvurdering/Testoppsett.kt
@@ -1,10 +1,5 @@
 package no.nav.helse.flex.juridiskvurdering
 
-import no.nav.helse.flex.juridiskvurdering.kafka.TESTDATA_RESET_TOPIC
-import org.amshove.kluent.shouldBeEmpty
-import org.apache.kafka.clients.consumer.Consumer
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -21,9 +16,6 @@ abstract class Testoppsett {
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @Autowired
-    lateinit var kafkaConsumer: Consumer<String, String?>
-
     companion object {
         init {
             KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.1.1")).also {
@@ -31,15 +23,5 @@ abstract class Testoppsett {
                 System.setProperty("KAFKA_BROKERS", it.bootstrapServers)
             }
         }
-    }
-
-    @BeforeAll
-    fun `Vi leser kafka topicet og feiler om noe eksisterer`() {
-        kafkaConsumer.subscribeHvisIkkeSubscribed(TESTDATA_RESET_TOPIC)
-        kafkaConsumer.hentProduserteRecords().shouldBeEmpty()
-    }
-    @AfterAll
-    fun `Vi leser topicet og feiler hvis noe finnes og slik at subklassetestene leser alt`() {
-        kafkaConsumer.hentProduserteRecords().shouldBeEmpty()
     }
 }
