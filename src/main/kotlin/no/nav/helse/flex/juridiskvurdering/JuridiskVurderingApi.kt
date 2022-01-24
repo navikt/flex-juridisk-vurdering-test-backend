3fe1eadff188873e4f3b8832910b46bcccb022f2
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-01-24 18:13:41 +0100

    jsonraw

--- src/main/kotlin/no/nav/helse/flex/juridiskvurdering/JuridiskVurderingApi.kt
+++ src/main/kotlin/no/nav/helse/flex/juridiskvurdering/JuridiskVurderingApi.kt
@@ -1,11 +1,14 @@
 package no.nav.helse.flex.juridiskvurdering
 
+import com.fasterxml.jackson.annotation.JsonRawValue
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+import java.time.OffsetDateTime
 
 @RestController
 @RequestMapping(value = ["/api"])
@@ -15,7 +18,19 @@ class JuridiskVurderingApi(
 
     @ResponseBody
     @GetMapping(value = ["/vurderinger/{fnr}"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun hentVurderinger(@PathVariable fnr: String): List<JuridiskVurderingDbRecord> {
-        return juridiskVurderingRepository.findByFnr(fnr)
+    fun hentVurderinger(@PathVariable fnr: String): List<VurderingResponse> {
+        return juridiskVurderingRepository.findByFnr(fnr).map { it.tilVurderingResponse() }
     }
+
+    private fun JuridiskVurderingDbRecord.tilVurderingResponse(): VurderingResponse =
+        VurderingResponse(fnr = fnr, opprettet = opprettet, vurdering = juridiskVurdering)
+
+
+    data class VurderingResponse(
+        val fnr: String,
+        val opprettet: OffsetDateTime,
+        @JsonRawValue
+        val vurdering: String
+    )
 }
+
